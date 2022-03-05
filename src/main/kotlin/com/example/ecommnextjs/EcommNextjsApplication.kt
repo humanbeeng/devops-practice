a4ada47ecd05c6d1fe05b1dc9da0f74a58395786
Nithin <thenithinraj@gmail.com>
2022-03-05 20:31:06 +0530

    Initial commit

---
+++ src/main/kotlin/com/example/ecommnextjs/EcommNextjsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ecommnextjs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommNextjsApplication
+
+fun main(args: Array<String>) {
+    runApplication<EcommNextjsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ecommnextjs/EcommNextjsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ecommnextjs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcommNextjsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
