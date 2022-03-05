1d3eb3fcbe9e7ac24b867664c8733272cdf1ccec
Nithin <thenithinraj@gmail.com>
2022-03-05 21:02:05 +0530

    Made test to fail to test build workflow

--- src/test/kotlin/com/example/ecommnextjs/EcommNextjsApplicationTests.kt
+++ src/test/kotlin/com/example/ecommnextjs/EcommNextjsApplicationTests.kt
@@ -1,5 +1,6 @@
 package com.example.ecommnextjs
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,6 +9,7 @@ class EcommNextjsApplicationTests {
 
     @Test
     fun contextLoads() {
+        Assertions.fail<String>("Oops test failed")
     }
 
 }
